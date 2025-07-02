a13f42cb389f087877dab67d1481093132fce94b
angelmaria3 <angelmariasunil@gmail.com>
2025-07-02 23:35:06 +0530

    Initial commit

--- password.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-charValues=string.ascii_letters + string.digits + string.punctuation
-password=""
-n=int(input("Enter the length of the password: "))
-for i in range(n):
-    password+=random.choice(charValues)
-print("Your password is:", password)
